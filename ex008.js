a85533bd512160f85503816c4181120c496149cf
Yago Andrade <yagoandradelima@outlook.com>
2023-11-21 12:35:42 -0300

    condições em js

---
+++ ex008.js
@@ -0,0 +1,13 @@
+console.log('Teste de console') //Para printar no console
+//Debug utilizando o node js
+var vel = 90
+
+if (vel > 80) {
+    console.log(`A velocidade do seu carro é ${vel}`)
+    console.log('Você foi multado!')
+} 
+
+else {
+    console.log(`Sua velocidade está abaixo do limite permitido! Parabéns.`)
+    console.log('Dirija sempre usando o cinto de segurança. Boa viagem!')
+}
