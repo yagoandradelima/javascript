a388cbbc7310b18c39675e66faf3810726b9beba
Yago Andrade <yagoandradelima@outlook.com>
2023-11-23 15:38:18 -0300

    atividade 04

---
+++ ambiente.js
@@ -0,0 +1,22 @@
+
+//Estrutura while
+c = 1
+while (c <= 6) {
+    console.log('Testando')
+    c++
+}
+
+
+//Estrutura do-while
+c = 1
+do {
+    console.log('Testando o Do-While')
+    c++
+} while (c <= 6)
+
+//Estrutura for
+for (var c = 1; c <= 12; c++){ //for (inicialização, teste lógico, incremento)
+    //Nessa inicialização eu posso declarar uma variável dentro dele
+    //Nesse teste lógico eu posso jogar um novo while ou do-while
+    console.log('Testando estrutura for')
+}
