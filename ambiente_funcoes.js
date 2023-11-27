81d2fd4e539b9bcf1ffcaf1e78413769445af9ee
Yago Andrade <yagoandradelima@outlook.com>
2023-11-27 18:11:14 -0300

    Create ambiente_funcoes.js

---
+++ ambiente_funcoes.js
@@ -0,0 +1,23 @@
+//função que exibe se o número é par ou ímpar
+function parimp(n){
+    if (n%2 == 0){
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+console.log(parimp(4))
+
+
+function soma(n1, n2) {
+    return n1 + n2
+}
+console.log(soma(2, 5))
+
+
+let v = function(x){
+    return x*2
+}
+console.log(v(5))
+
+//Comentários dentro do obsidian
