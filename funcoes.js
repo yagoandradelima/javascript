050d14a3adfbf539332badb7b49321308ca6ed16
Yago Andrade <yagoandradelima@outlook.com>
2023-12-01 14:35:47 -0300

    update

---
+++ funcoes.js
@@ -0,0 +1,28 @@
+//Fiz uma function simples literal
+function soma (num1, num2){
+    const sum = num1 + num2;
+    return sum
+}
+
+console.log(soma(10, 5))
+
+
+//Fiz uma arrow function ou uma expressão de função
+const subt = (num1, num2) => {
+    const sub = num1 - num2; 
+    return sub
+}
+
+console.log(subt(2, 1))
+
+
+//Mas eu posso reduzir o código anterior!!
+const subtra = (num1, num2) => {
+    return num1 - num2; 
+}
+
+console.log(subtra(10, 2))
+
+
+//Mas eu psoso reduzir AINDA mais
+const subtracao = (num1, num2) => num1 + num2
